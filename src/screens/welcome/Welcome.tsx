ba84b44ae45405a7ea7182d996822da3a1d5844b
Dawid Matyjasik <dawidspisak@gmail.com>
2024-10-02 20:33:59 +0200

    feat: introduce react-navigation

---
+++ src/screens/welcome/Welcome.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import { Button } from "react-native-paper";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+const Welcome = () => {
+  return (
+    <SafeAreaView>
+      <Button mode="contained">Dalej</Button>
+    </SafeAreaView>
+  );
+};
+
+export default Welcome;
