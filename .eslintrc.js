1e2b506aaac457b32a81aa4fd45ff27e822cf30a
Dawid Matyjasik <dawidspisak@gmail.com>
2024-10-02 18:40:19 +0200

    feat: eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,29 +1,17 @@
 module.exports = {
   root: true,
-  extends: [
-    "@react-native",
-    "airbnb",
-    "airbnb-typescript",
-    "airbnb/hooks",
-    "prettier",
-    "plugin:prettier/recommended",
-    "plugin:import/recommended",
-    "plugin:import/typescript",
-    "plugin:jsx-a11y/recommended",
-  ],
+  extends: ["@react-native", "prettier"],
   plugins: ["react", "@typescript-eslint", "import", "prettier", "jsx-a11y"],
   parserOptions: {
     project: ["./tsconfig.json"],
   },
   rules: {
-
     "import/prefer-default-export": "off",
     "react/require-default-props": "off",
     "react/react-in-jsx-scope": "off",
     "react/jsx-uses-react": "off",
     "react/jsx-props-no-spreading": "off",
     "react/jsx-filename-extension": [1, { extensions: [".js", ".jsx", ".ts", ".tsx"] }],
-    "@typescript-eslint/consistent-type-imports": "error",
     "react/function-component-definition": "off",
     "react/style-prop-object": "warn",
     "consistent-return": "off",
@@ -53,6 +41,14 @@ module.exports = {
         "newlines-between": "always",
       },
     ],
-    "@typescript-eslint/no-use-before-define": ["error", { "functions": true, "classes": true, "variables": false }], // disable the rule for variables, but 
+    "@typescript-eslint/no-use-before-define": ["error", { functions: true, classes: true, variables: false }],
+  },
+  settings: {
+    "import/resolver": {
+      node: {
+        extensions: [".js", ".jsx", ".ts", ".tsx"],
+        moduleDirectory: ["node_modules", "src/"],
+      },
+    },
   },
 };
