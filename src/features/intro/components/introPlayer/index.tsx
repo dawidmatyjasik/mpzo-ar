5077496c386fe878952eb861a3eb8fc719705c7b
Dawid Matyjasik <dawidspisak@gmail.com>
2024-10-06 16:49:42 +0200

    feat: play button

--- src/features/intro/components/introPlayer/index.tsx
+++ src/features/intro/components/introPlayer/index.tsx
@@ -1,7 +1,8 @@
 import React from "react";
-import { View, StyleSheet } from "react-native";
+import { View, StyleSheet, TouchableOpacity } from "react-native";
 
 import { MD3Colors } from "react-native-paper/lib/typescript/types";
+import IoniconsPlay from "react-native-vector-icons/Ionicons";
 
 import { useAppTheme } from "features/theme/hooks";
 
@@ -9,7 +10,13 @@ const IntroPlayer = () => {
   const { colors } = useAppTheme();
   const styles = getStyles(colors);
 
-  return <View style={styles.player} />;
+  return (
+    <TouchableOpacity style={styles.player}>
+      <View style={styles.playerWrapper}>
+        <IoniconsPlay name="play" size={28} color={colors.primary} style={styles.playerIcon} />
+      </View>
+    </TouchableOpacity>
+  );
 };
 
 const getStyles = (colors: MD3Colors) =>
@@ -24,6 +31,17 @@ const getStyles = (colors: MD3Colors) =>
       borderWidth: 1,
       borderColor: colors.onPrimaryContainer,
       backgroundColor: colors.onPrimary,
+    },
+    playerWrapper: {
+      position: "relative",
+      width: "100%",
+      height: "100%",
+    },
+    playerIcon: {
+      position: "absolute",
+      alignSelf: "center",
+      bottom: "50%",
+      transform: [{ translateY: 14 }],
     },
   });
 
