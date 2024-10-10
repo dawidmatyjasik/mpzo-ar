993a633cec78d3cf54aabbdbe5e62f7cf9535640
Dawid Matyjasik <dawidspisak@gmail.com>
2024-10-10 19:01:25 +0200

    feat: dynamic header

--- src/screens/hero/index.tsx
+++ src/screens/hero/index.tsx
@@ -1,6 +1,7 @@
-import React, { FC } from "react";
+import React, { FC, useEffect } from "react";
 import { Image, ScrollView, StyleSheet, View } from "react-native";
 
+import { useTranslation } from "react-i18next";
 import { Text } from "react-native-paper";
 import { SafeAreaView } from "react-native-safe-area-context";
 
@@ -9,16 +10,22 @@ import { MapStackScreenProps } from "features/navigation/components/mapNavigatio
 
 type HeroScreenProps = MapStackScreenProps<"HeroScreen">;
 
-const HeroScreen: FC<HeroScreenProps> = ({ route }) => {
+const HeroScreen: FC<HeroScreenProps> = ({ route, navigation }) => {
   const { description, name } = route.params;
   const styles = getStyles();
+  const { t } = useTranslation();
+
+  useEffect(() => {
+    navigation.setOptions({ title: t(name) });
+  }, [name, navigation, t]);
+
   return (
     <SafeAreaView style={styles.container} edges={["right", "left"]}>
       <ScrollView>
         <Image source={MapImage} />
         <View style={styles.content}>
-          <Text variant="headlineLarge">{name}</Text>
-          <Text variant="bodyLarge">{description}</Text>
+          <Text variant="headlineLarge">{t(name)}</Text>
+          <Text variant="bodyLarge">{t(description)}</Text>
         </View>
       </ScrollView>
     </SafeAreaView>
