9443452231ee7974abe3474db9e608409f811f19
Dawid Matyjasik <dawidspisak@gmail.com>
2024-10-14 23:00:39 +0200

    feat: custom hero header

--- src/screens/hero/index.tsx
+++ src/screens/hero/index.tsx
@@ -1,30 +1,38 @@
-import React, { FC, useEffect } from "react";
-import { Image, ScrollView, StyleSheet, View } from "react-native";
+import React, { FC } from "react";
+import { ScrollView, StyleSheet, View } from "react-native";
 
+import { Header } from "@react-navigation/elements";
 import { useTranslation } from "react-i18next";
 import { Text } from "react-native-paper";
-import { SafeAreaView } from "react-native-safe-area-context";
+import { SafeAreaView, useSafeAreaInsets } from "react-native-safe-area-context";
 
-import MapImage from "assets/png/step1-image.png";
+import BackButton from "features/common/components/backButton";
+import { TABS_HEIGHT } from "features/common/consts";
+import { useSizes } from "features/common/hooks/useSizes";
 import { MapStackScreenProps } from "features/navigation/components/mapNavigation";
+import HeroVideo from "features/video/components";
 
 type HeroScreenProps = MapStackScreenProps<"HeroScreen">;
 
-const HeroScreen: FC<HeroScreenProps> = ({ route, navigation }) => {
+const HeroScreen: FC<HeroScreenProps> = ({ route }) => {
   const { description, name } = route.params;
-  const styles = getStyles();
-  const { t } = useTranslation();
 
-  useEffect(() => {
-    navigation.setOptions({ title: t(name) });
-  }, [name, navigation, t]);
+  const styles = getStyles();
+
+  const { t } = useTranslation();
+  const { bottom } = useSafeAreaInsets();
+  const { height } = useSizes();
 
   return (
-    <SafeAreaView style={styles.container} edges={["right", "left"]}>
+    <SafeAreaView style={[{ height: height - bottom - TABS_HEIGHT }]} edges={["right", "left"]}>
+      <View style={styles.headerContainer}>
+        <Header title={t(name)} headerLeft={() => <BackButton />} headerBackgroundContainerStyle={styles.headerSize} />
+      </View>
+      <HeroVideo />
       <ScrollView>
-        <Image source={MapImage} />
         <View style={styles.content}>
           <Text variant="headlineLarge">{t(name)}</Text>
+          <Text variant="bodyLarge">{t(description)}</Text>
           <Text variant="bodyLarge">{t(description)}</Text>
         </View>
       </ScrollView>
@@ -34,12 +42,15 @@ const HeroScreen: FC<HeroScreenProps> = ({ route, navigation }) => {
 
 const getStyles = () =>
   StyleSheet.create({
-    container: {
-      flex: 1,
-    },
     content: {
       padding: 32,
       gap: 16,
+    },
+    headerContainer: {
+      paddingHorizontal: 16,
+    },
+    headerSize: {
+      height: 98,
     },
   });
 
