dbf5e573a90929abb803641d0ac5c8163c5d362e
Dawid Matyjasik <dawidspisak@gmail.com>
2024-10-17 18:16:06 +0200

    feat: restart modal

--- src/features/settings/components/restartModal/index.tsx
+++ src/features/settings/components/restartModal/index.tsx
@@ -1,7 +1,10 @@
 import React, { Dispatch, SetStateAction } from "react";
-import { Text } from "react-native";
+import { StyleSheet, View } from "react-native";
 
-import { Modal, Portal } from "react-native-paper";
+import { useTranslation } from "react-i18next";
+import { Modal, Portal, Text } from "react-native-paper";
+
+import CustomButton from "components/customButton";
 
 interface RestartModalProps {
   isVisible: boolean;
@@ -9,15 +12,39 @@ interface RestartModalProps {
 }
 
 const RestartModal = ({ isVisible, setIsVisible }: RestartModalProps) => {
-  const containerStyle = { backgroundColor: "white", padding: 20 };
-
+  const { t } = useTranslation();
   return (
     <Portal>
-      <Modal visible={isVisible} onDismiss={() => setIsVisible(false)} contentContainerStyle={containerStyle}>
-        <Text>Example Modal. Click outside this area to dismiss.</Text>
+      <Modal visible={isVisible} onDismiss={() => setIsVisible(false)} contentContainerStyle={styles.container}>
+        <Text variant="headlineSmall" style={styles.text}>
+          {t("restart.modalTitle")}
+        </Text>
+        <Text variant="bodyLarge" style={styles.text}>
+          {t("restart.modalDescription")}
+        </Text>
+        <View style={styles.actionsContainer}>
+          <CustomButton mode="contained"> {t("restart.confirm")}</CustomButton>
+          <CustomButton mode="outlined"> {t("restart.cancel")}</CustomButton>
+        </View>
       </Modal>
     </Portal>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "white",
+    marginHorizontal: 24,
+    padding: 32,
+    borderRadius: 16,
+    gap: 16,
+  },
+  text: {
+    textAlign: "center",
+  },
+  actionsContainer: {
+    gap: 12,
+  },
+});
 
 export default RestartModal;
